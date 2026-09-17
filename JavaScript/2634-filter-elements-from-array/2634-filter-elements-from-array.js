60b5e629da44b7bd5bc102beb9fe5b082d1048a1
Aman Bam <217863286+Aman-Bam@users.noreply.github.com>
2026-09-17 08:58:28 +0530

    Time: 41 ms (69.84%), Space: 52.8 MB (86.19%) - LeetHub

---
+++ JavaScript/2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,9 @@
+var filter = function(arr, fn) {
+    let newArr = []
+    arr.forEach((value, index) => {
+        if(fn(value, index)) {
+            newArr.push(value)
+        }
+    })
+    return newArr
+};
