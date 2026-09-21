c955e18fabf48fd687ea47566618d8566f4af1b4
Aman Bam <217863286+Aman-Bam@users.noreply.github.com>
2026-09-21 17:15:41 +0530

    Time: 26 ms (46.72%), Space: 78.6 MB (77.35%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.js
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.js
@@ -1,8 +1,4 @@
 var containsDuplicate = function(nums) {
-    const map = new Map()
-    for (const n of nums) {
-        if (map.has(n)) return true
-        map.set(n, true)
-    }
-    return false
+    const set = new Set(nums)
+    return set.size != nums.length
 };
